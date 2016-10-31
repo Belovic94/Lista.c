44a2a129162b7acf5d2f8129e08993594788b5d3
Victor Belosevich <victor.belosevich94@gmail.com>
2016-10-31 16:22:13 -0300

    Falta arreglar algunas pruebas

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -7,51 +7,13 @@
 #define CANT_ELEM 1000
 #define RANGO_RAND 100 //rango del random.
 
-void pruebas_lista_no_creada(void){
-  lista_t* lista = NULL;
-  int valor = 5;
-  lista_iter_t* l_iter = NULL;
-
-  print_test("Insertar primero en una lista no creada(false)", !lista_insertar_primero(lista, &valor));
-  print_test("Borrar primero en una lista no creada(NULL)", !lista_borrar_primero(lista));
-  print_test("Ver primero en una lista no creada(NULL)", !lista_ver_primero(lista));
-  l_iter = lista_iter_crear(lista);
-  print_test("Intentar crear iterador sin una lista creada(NULL)", !l_iter);
-  print_test("Obtener el largo de una lista no creada(0)", lista_largo(lista) == 0);
-  print_test("Insertar ultimo en una lista no creada(false)", !lista_insertar_ultimo(lista, &valor));
-}
-
-void pruebas_lista_vacia(void){
-  lista_t* lista = lista_crear();
-  int valor1 = 5;
-  lista_iter_t* l_iter;
-
-  print_test("Lista creada exitosamente", lista);
-  print_test("Ver primero en una lista vacia(NULL)", !lista_ver_primero(lista));
-  l_iter = lista_iter_crear(lista);
-  print_test("Crear iterador en lista vacia(iter)", l_iter);
-  lista_iter_destruir(l_iter);
-  print_test("Destruir iterador creado anteriormente(true)", true);
-  print_test("Insertar primero en una lista vacia(true)", lista_insertar_primero(lista, &valor1));
-  print_test("Ver primero en una lista(valor)", *(int*)lista_ver_primero(lista) == valor1);
-  print_test("Obtener el largo de una lista(1)", lista_largo(lista) == 1);
-  l_iter = lista_iter_crear(lista);
-  print_test("Crear iterador en lista no vacia(iter)", l_iter);
-  print_test("Iterador apunta al primer elemento de la lista(true)",
-                  *(int*)lista_iter_ver_actual(l_iter) == *(int*)lista_ver_primero(lista));
-  print_test("Borrar primero en una lista (valor1)", lista_borrar_primero(lista));
-  print_test("Obtener el largo de una lista vacia(0)", lista_largo(lista) == 0);
-  lista_iter_destruir(l_iter);
-  print_test("Destruir iterador creado anteriormente(true)", true);
-  print_test("Insertar ultimo en una lista (true)", lista_insertar_ultimo(lista, &valor1));
-  print_test("Obtener el largo de una lista(1)", lista_largo(lista) == 1);
-  lista_destruir(lista, NULL);
-  print_test("Destruyo la lista con datos adentro", true);
-}
+/* ******************************************************************
+ *                        FUNCIONES AUXILIARES
+ * *****************************************************************/
 
 /*Recibe una lista y un puntero.
 Devuelve true si el dato esta en la lista, en caso contrario false.*/
-bool buscar_con_iterador(lista_t* lista, void* dato){
+bool lista_pertenece(lista_t* lista, void* dato){
   lista_iter_t* l_iter = lista_iter_crear(lista);
   while(!lista_iter_al_final(l_iter)){
     if(lista_iter_ver_actual(l_iter) == dato){
@@ -62,6 +24,23 @@ bool buscar_con_iterador(lista_t* lista, void* dato){
   }
   return false;
 }
+
+void* lista_obtener(lista_t* lista, int pos){
+  if(pos >= lista_largo(lista)) return NULL;
+  lista_iter_t* l_iter = lista_iter_crear(lista);
+  if(!l_iter) return NULL;
+  for(int i = 0; i <= lista_largo(lista); i++){
+    if(i == pos){
+      void *dato = lista_iter_ver_actual(l_iter);
+      lista_iter_destruir(l_iter);
+      return dato;
+    }
+    lista_iter_avanzar(l_iter);
+  }
+  return NULL;
+}
+
+
 /*Recibe una lista.
 Devuelve elimina la ultima posicion de la lista y devuelve el dato.*/
 void* eliminar_ultimo_elemento(lista_t* lista){
@@ -77,10 +56,13 @@ void* eliminar_ultimo_elemento(lista_t* lista){
   }
   return NULL;
 }
+
 /*Recibe una lista, un puntero y una posicion.
 Devuelve true si se pudo insertar el dato en la posicion especificada de la lista, en caso contrario false.*/
 bool insertar_en_lista(lista_t* lista, void* dato, size_t pos){
+  if(pos >= lista_largo(lista)) return false;
   lista_iter_t* l_iter = lista_iter_crear(lista);
+  if(!l_iter) return false;
   bool insercion_valida = false;
   for(int i = 0; i <= lista_largo(lista); i++){
     if(i == pos){
@@ -93,8 +75,154 @@ bool insertar_en_lista(lista_t* lista, void* dato, size_t pos){
   return insercion_valida;
 }
 
+/*Llena un vector con valores enteros aleatorios entre 0 y 100.*/
+void llenar_vector(int* vector, int largo){
+  srand((int)time(NULL));
+  for (int i = 0; i < largo; i++){
+    vector[i] = rand() % (RANGO_RAND +1);
+  }
+}
 
-void pruebas_lista_elementos_distintos(void){
+/*Agrega una cierta cantidad de valores enteros aleatorios a una lista*/
+bool agregar_valores_lista(lista_t *lista, int *vector, int largo){
+  if(!lista || !vector){
+    return false;
+  }
+  for(int i = 0; i < largo; i++){
+    if(!lista_insertar_ultimo(lista, &vector[i])){
+      return false;
+    }
+  }
+  return true;
+}
+
+/*Vacia la lista por medio de un iterador externo*/
+void vaciar_lista(lista_t *lista){
+  lista_iter_t *iter = lista_iter_crear(lista);
+  while(!lista_iter_al_final(iter)){
+    lista_iter_borrar(iter);
+  }
+  lista_iter_destruir(iter);
+}
+/**********************FUNCIONES ITERADOR INTERNO********************************/
+
+/*Calcula el maximo entre dos numeros*/
+bool calcular_maximo(void *dato , void *extra){
+  int *numero = dato, *maximo = extra;
+  if(*numero > *maximo){
+    *maximo = *numero;
+  }
+  return true;
+}
+
+/*Cuenta la cantidad de caracteres de una palabra*/
+bool cantidad_caracteres(void *dato, void *extra){
+  char *palabra = dato;
+  int *total_caracteres = extra;
+
+  for(int i=0; palabra[i] != '\0'; i++){
+    *total_caracteres += 1;
+  }
+  return true;
+}
+
+
+
+/* ******************************************************************
+ *                        PRUEBAS DE LA LISTA
+ * *****************************************************************/
+
+void pruebas_lista_no_creada(void){
+  lista_t* lista = NULL;
+  int valor = 5;
+
+  printf("\n***PRUEBAS LISTA NO CREADA***\n");
+  print_test("Insertar primero en una lista no creada(false)", !lista_insertar_primero(lista, &valor));
+  print_test("Borrar primero en una lista no creada(NULL)", !lista_borrar_primero(lista));
+  print_test("Ver primero en una lista no creada(NULL)", !lista_ver_primero(lista));
+  print_test("Ver ultimo en una lista no creada(NULL)", !lista_ver_ultimo(lista));
+  print_test("Obtener el largo de una lista no creada(0)", lista_largo(lista) == 0);
+  print_test("Ver si esta vacia en una lista no creada(true)", lista_esta_vacia(lista));
+  print_test("Insertar primero en una lista no creada(false)", !lista_insertar_primero(lista, &valor));
+  print_test("Insertar ultimo en una lista no creada(false)", !lista_insertar_ultimo(lista, &valor));
+
+}
+
+void pruebas_iterador_lista_no_creada(void){
+  lista_t* lista = NULL;
+  int valor = 5;
+  lista_iter_t* l_iter = lista_iter_crear(lista);
+
+  printf("\n***PRUEBAS ITERADOR LISTA NO CREADA***\n");
+  print_test("Intentar crear iterador sin una lista creada(NULL)", !l_iter);
+  print_test("Avanzar sin un iterador creado", !lista_iter_avanzar(l_iter));
+  print_test("Ver si el iterador esta al final(true)", lista_iter_al_final(l_iter));
+  print_test("Ver elemento actual(NULL)", !lista_iter_ver_actual(l_iter));
+  print_test("Borrar un elemento de la lista no creada(NULL)", !lista_iter_borrar(l_iter));
+  print_test("Insertar un elemento en una lista no creada(false)", !lista_iter_insertar(l_iter, &valor));
+}
+
+
+void pruebas_lista_vacia(void){
+  lista_t* lista = lista_crear();
+  int valor1 = 5;
+  int valor2 = 10;
+
+  printf("\n***PRUEBAS LISTA VACIA***\n");
+  print_test("Lista creada exitosamente", lista);
+  print_test("Ver primero en una lista vacia(NULL)", !lista_ver_primero(lista));
+  print_test("Ver ultimo en una lista vacia(NULL)", !lista_ver_ultimo(lista));
+  print_test("Borrar primero en una lista vacia(NULL)", !lista_borrar_primero(lista));
+  print_test("Ver si esta vacia en una lista vacia(true)", lista_esta_vacia(lista));
+  print_test("Largo de una lista vacia(0)", lista_largo(lista) == 0);
+
+  print_test("Insertar primero en una lista vacia(true)", lista_insertar_primero(lista, &valor1));
+  print_test("Ver primero en una lista(valor)", *(int*)lista_ver_primero(lista) == valor1);
+  print_test("Ver último en una lista(valor)", *(int*)lista_ver_ultimo(lista) == valor1);
+  print_test("Obtener el largo de una lista(1)", lista_largo(lista) == 1);
+  print_test("Borrar primero en una lista (valor)", *(int*)lista_borrar_primero(lista) == valor1);
+  print_test("Obtener el largo de una lista(0)", lista_largo(lista) == 0);
+  print_test("Ver primero en una lista vacia(NULL)", !lista_ver_primero(lista));
+  print_test("Ver ultimo en una lista vacia(NULL)", !lista_ver_ultimo(lista));
+
+  print_test("Insertar ultimo en una lista (true)", lista_insertar_ultimo(lista, &valor2));
+  print_test("Ver primero en una lista(valor)", *(int*)lista_ver_primero(lista) == valor2);
+  print_test("Ver último en una lista(valor)", *(int*)lista_ver_ultimo(lista) == valor2);
+  print_test("Obtener el largo de una lista(1)", lista_largo(lista) == 1);
+  print_test("Borrar primero en una lista (valor)", *(int*)lista_borrar_primero(lista) == valor2);
+  print_test("Obtener el largo de una lista(0)", lista_largo(lista) == 0);
+  print_test("Ver primero en una lista vacia(NULL)", !lista_ver_primero(lista));
+  print_test("Ver ultimo en una lista vacia(NULL)", !lista_ver_ultimo(lista));
+
+  lista_destruir(lista, NULL);
+  print_test("Destruyo la lista vacia", true);
+
+
+}
+
+
+void pruebas_iterador_lista_vacia(void){
+  lista_t* lista = lista_crear();
+  int valor = 5;
+  lista_iter_t* l_iter = lista_iter_crear(lista);
+
+  printf("\n***PRUEBAS ITERADOR LISTA VACIA***\n");
+  print_test("Crear iterador en lista vacia(iter)", l_iter);
+  print_test("Avanzar iterador en lista vacia(false)", !lista_iter_avanzar(l_iter));
+  print_test("Ver si el iterador esta al final(true)", lista_iter_al_final(l_iter));
+  print_test("Ver elemento actual(NULL)", !lista_iter_ver_actual(l_iter));
+  print_test("Borrar un elemento de la lista vacia con iterador(NULL)", !lista_iter_borrar(l_iter));
+  print_test("Insertar elemento en la lista vacia con iterador(true)", lista_iter_insertar(l_iter, &valor));
+
+  lista_iter_destruir(l_iter);
+  print_test("Destruir iterador de lista vacia(true)", true);
+  lista_destruir(lista, NULL);
+  print_test("Destruyo la lista vacia", true);
+}
+
+
+
+void pruebas_iterardor_externo(void){
   lista_t* lista = lista_crear();
   lista_iter_t* l_iter;
   //valores que ocupan memoria dinámica
@@ -114,18 +242,20 @@ void pruebas_lista_elementos_distintos(void){
   *valorint2 = 34;
   *valor_ingresado = 5;
 
+  printf("\n***PRUEBAS ITERADOR EXTERNO***\n");
   print_test("Lista creada exitosamente", lista);
   print_test("Insertar primero en una lista (true)", lista_insertar_primero(lista, valordou1));
   print_test("Insertar primero en una lista (true)", lista_insertar_primero(lista, valordou2));
   print_test("Insertar primero en una lista (true)", lista_insertar_primero(lista, valorlon1));
   print_test("Insertar primero en una lista (true)", lista_insertar_primero(lista, valorlon2));
-  print_test("Insertar primero en una lista (true)", lista_insertar_ultimo(lista, valorcha1));
-  print_test("Insertar primero en una lista (true)", lista_insertar_ultimo(lista, valorcha2));
-  print_test("Insertar primero en una lista (true)", lista_insertar_ultimo(lista, valorint1));
-  print_test("Insertar primero en una lista (true)", lista_insertar_ultimo(lista, valorint2));
+  print_test("Insertar ultimo en una lista (true)", lista_insertar_ultimo(lista, valorcha1));
+  print_test("Insertar ultimo en una lista (true)", lista_insertar_ultimo(lista, valorcha2));
+  print_test("Insertar ultimo en una lista (true)", lista_insertar_ultimo(lista, valorint1));
+  print_test("Insertar ultimo en una lista (true)", lista_insertar_ultimo(lista, valorint2));
+
   //pruebo el iterador externo
   print_test("Pruebo buscar un elemento en la lista con el iterador(true)",
-                                  buscar_con_iterador(lista, valorcha1));
+                                  lista_pertenece(lista, valorcha1));
   print_test("Pruebo borrar el ultimo elemento en la lista con el iterador(valorint2)",
                               *(int*)eliminar_ultimo_elemento(lista) == *valorint2);
   print_test("Veo si el largo de la lista cambio (7)", lista_largo(lista) == 7);
@@ -133,7 +263,9 @@ void pruebas_lista_elementos_distintos(void){
                                 insertar_en_lista(lista, valor_ingresado, 6));
   print_test("Veo si el largo de la lista cambio (8)", lista_largo(lista) == 8);
   print_test("Pruebo buscar el elemento recien ingresado a la lista con el iterador(true)",
-                                  buscar_con_iterador(lista, valor_ingresado));
+                                  lista_pertenece(lista, valor_ingresado));
+  print_test("Busco el elemento en la posicion 5", *(char*)lista_obtener(lista, 5) == *valorcha2);
+
   l_iter = lista_iter_crear(lista);
   print_test("Creo un iterador de la lista con elementos(true)", l_iter);
   print_test("Avanzo el iterador (true)", lista_iter_avanzar(l_iter));
@@ -164,54 +296,6 @@ void pruebas_lista_elementos_distintos(void){
   print_test("Destruir iterador creado anteriormente(true)", true);
   lista_destruir(lista, free);//pruebo la funcion destructora
   print_test("Destruyo la lista con datos adentro", true);
-}
-/*Llena un vector con valores enteros aleatorios entre 0 y 100.*/
-void llenar_vector(int* vector, int largo){
-  srand((int)time(NULL));
-  for (int i = 0; i < largo; i++){
-    vector[i] = rand() % (RANGO_RAND +1);
-  }
-}
-/*Agrega una cierta cantidad de valores enteros aleatorios a una lista*/
-bool agregar_valores_lista(lista_t *lista, int *vector, int largo){
-  if(!lista || !vector){
-    return false;
-  }
-  for(int i = 0; i < largo; i++){
-    if(!lista_insertar_ultimo(lista, &vector[i])){
-      return false;
-    }
-  }
-  return true;
-}
-
-/*Vacia la lista por medio de un iterador externo*/
-void vaciar_lista(lista_t *lista){
-  lista_iter_t *iter = lista_iter_crear(lista);
-  while(!lista_iter_al_final(iter)){
-    lista_iter_borrar(iter);
-  }
-  lista_iter_destruir(iter);
-}
-
-/*Calcula el maximo entre dos numeros*/
-bool calcular_maximo(void *dato , void *extra){
-  int *numero = dato, *maximo = extra;
-  if(*numero > *maximo){
-    *maximo = *numero;
-  }
-  return true;
-}
-
-/*Cuenta la cantidad de caracteres de una palabra*/
-bool cantidad_caracteres(void *dato, void *extra){
-  char *palabra = dato;
-  int *total_caracteres = extra;
-
-  for(int i=0; palabra[i] != '\0'; i++){
-    *total_caracteres += 1;
-  }
-  return true;
 }
 
 void pruebas_iterardor_interno(void){
@@ -245,7 +329,9 @@ void pruebas_iterardor_interno(void){
 }
 void pruebas_lista_alumno(void){
   pruebas_lista_no_creada();
+  pruebas_iterador_lista_no_creada();
   pruebas_lista_vacia();
-  pruebas_lista_elementos_distintos();
+  pruebas_iterador_lista_vacia();
+  pruebas_iterardor_externo();
   pruebas_iterardor_interno();
 }
